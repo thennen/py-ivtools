dcb01526e6c2e23848a1678ec406b4cfca98f840
Moritz von Witzleben <moritz.witzleben@rwth-aachen.de>
2018-04-05 16:49:10 +0200

    removed conflict and added two_channel option

--- scripts/Keithley_tek_bias_tee.py
+++ scripts/Keithley_tek_bias_tee.py
@@ -40,54 +40,38 @@ iplots.plotters = [[0, plot0],
                    [3, plot3]]
              
 iplots.newline()
-<<<<<<< HEAD
-def pcm_measurement(voltage = -0.2, points = 1000 ,interval = 0.2):
-=======
-def pcm_measurement(amplitude = 10, bits = 256, sourceVA = -0.2, points = 250, interval = 0.1, samplename, samplepad):
->>>>>>> 27e9a37b012eae07965b739af98cc1003b3e8e22
+
+def pcm_measurement(samplename, samplepad, amplitude = 10, bits = 256, sourceVA = -0.2, points = 250, interval = 0.1, trigger = -0.3, two_channel = False):
     number_of_events =0
     data = {}
     data['t_scope'] = []
     data['v_pulse'] = []
     data['v_answer'] = []
     data['t_event'] = []
-<<<<<<< HEAD
-    iplots.show()
-
-    datafolder = 'C:/Messdaten/CPW6/x06y13/'
-
-    k.it(sourceVA = voltage, sourceVB = 0, points = points, interval = interval, rangeI = 0, limitI = 1, nplc = 1)
-=======
     data['amplitude'] = amplitude
     data['bits'] = bits
-    iplots.show()
+    iplots.show()    
 
     datafolder = 'C:/Messdaten/' + samplename + '/' + samplepad + '/'
 
     k.it(sourceVA = sourceVA, sourceVB = 0, points = points, interval = interval, rangeI = 0, limitI = 1, nplc = 1)
->>>>>>> 27e9a37b012eae07965b739af98cc1003b3e8e22
 
     ttx.inputstate(1, False)
     ttx.inputstate(2, True)
     ttx.inputstate(3, False)
-<<<<<<< HEAD
-    ttx.inputstate(4, True)
-    ttx.scale(2, 0.05)
-    ttx.scale(4, 0.4)
-    ttx.position(1, 2)
-    ttx.position(4, 4)
-    ttx.change_samplerate_and_recordlength(100e9, 5000)
-    ttx.arm(source = 4, level = -0.3, edge = 'e')
-=======
-    ttx.inputstate(4, False)
+    if two_channel:
+        ttx.inputstate(4, True)
+        ttx.scale(4, 0.4)
+        ttx.position(4, 4)
+    else:
+        ttx.inputstate(4, False)
     ttx.scale(2, 0.03)
-    #ttx.scale(4, 0.4)
     ttx.position(2, 1)
-    #ttx.position(4, 4)
     ttx.change_samplerate_and_recordlength(100e9, 5000)
-    ttx.arm(source = 2, level = -0.05, edge = 'e')
->>>>>>> 27e9a37b012eae07965b739af98cc1003b3e8e22
-
+    if two_channel:
+        ttx.arm(source = 4, level = trigger, edge = 'e')
+    else:
+        ttx.arm(source = 2, level = trigger, edge = 'e')
     while not k.done():
         data.update(k.get_data())
         if ttx.triggerstate():
@@ -99,21 +83,17 @@ def pcm_measurement(amplitude = 10, bits = 256, sourceVA = -0.2, points = 250, i
             
             time_array = data['t']
             data['t_scope'].append(data_scope1['t_ttx'])
-<<<<<<< HEAD
-            data['v_pulse'].append(data_scope1['V_ttx'])
-=======
-            #data['v_pulse'].append(data_scope1['V_ttx'])
->>>>>>> 27e9a37b012eae07965b739af98cc1003b3e8e22
+            if two_channel:
+                data['v_pulse'].append(data_scope1['V_ttx'])
             data['v_answer'].append(data_scope2['V_ttx'])
             '''Moritz: last current data point measured after last trigger event so the entry one before
              will be used as time reference (-2 instead of -1, which be the last entry)'''
             data['t_event'].append(time_array[len(time_array)-2])
             print(time_array[len(time_array)-2])
-<<<<<<< HEAD
-            ttx.arm(source = 4, level = -0.3, edge = 'e')
-=======
-            ttx.arm(source = 2, level = -0.05, edge = 'e')
->>>>>>> 27e9a37b012eae07965b739af98cc1003b3e8e22
+            if two_channel:
+                ttx.arm(source = 4, level = trigger, edge = 'e')
+            else:
+                ttx.arm(source = 2, level = trigger, edge = 'e')
 
         iplots.updateline(data)
 
@@ -124,11 +104,6 @@ def pcm_measurement(amplitude = 10, bits = 256, sourceVA = -0.2, points = 250, i
     ttx.disarm()
     savedata(data)
 
-<<<<<<< HEAD
 def eval_ultrafast(filename):
     data = pd.read_pickle(filename)
         
-=======
-
-
->>>>>>> 27e9a37b012eae07965b739af98cc1003b3e8e22
