3d536095ec1304146db3344b708e660b1104ecf0
Moritz von Witzleben <moritz.witzleben@rwth-aachen.de>
2019-08-22 10:14:48 +0200

    test

--- scripts/code_schnipsel.py
+++ scripts/code_schnipsel.py
@@ -1,4 +1,4 @@
-######### erlaubt es alle daten nach namen zu sortieren, sodass das erstelldatum passt ############
+######### erlaubt es alle daten nach namen zu sortieren, sodass das erstelldatum passt 
 
 os.chdir('X:/emrl/Pool/Bulletin/Witzleben/Messungen/RF/Rohdaten/RF_Messplatz/AH2/x10y08_RESET/sortiert/0dB/')
 files = os.listdir()
@@ -6,4 +6,4 @@ for f in files:
     if len(f) == 10:
         filename= f[:5] + '0' + f[5:]
         print(filename)
-        os.rename(f, filename)
+        os.rename(f, filename) 
