9bc080539c7e4ce7b7e2d0878234daefbee59470
Munoz <a.munoz@iwe.rwth-aachen.de>
2020-07-09 09:27:37 +0200

    AUTOCOMMIT

--- tests/log_test.py
+++ tests/log_test.py
@@ -1,15 +1,17 @@
 import ivtools
 import logging
 
-log = logging.getLogger('my_logger')
+if __name__ == '__main__':
 
-log.debug("This is a 'debug' message")
-log.info("This is an 'info' message")
-log.warning("This is a 'warning' message")
-log.error("This is an 'error' message")
-log.critical("This is a 'critical' message")
-log.instruments("This is an 'instruments' message")
-log.io("This is an 'io' message")
-log.plots("This is a 'plot' message")
-log.analysis("This is an 'analysis' message")
-log.interactive("This is an 'interactive' message")
+    log = logging.getLogger('my_logger')
+
+    log.debug("This is a 'debug' message")
+    log.info("This is an 'info' message")
+    log.warning("This is a 'warning' message")
+    log.error("This is an 'error' message")
+    log.critical("This is a 'critical' message")
+    log.instruments("This is an 'instruments' message")
+    log.io("This is an 'io' message")
+    log.plots("This is a 'plot' message")
+    log.analysis("This is an 'analysis' message")
+    log.interactive("This is an 'interactive' message")
