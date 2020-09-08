e8f36aaa8269c407e9403202d778e1cb92e02c9a
Tyler Hennen <tyler@hennen.us>
2020-09-08 18:01:52 +0200

    Worked a little on the TeoSystem comments

--- ivtools/instruments/TeoSystem.py
+++ ivtools/instruments/TeoSystem.py
@@ -15,17 +15,36 @@ class TeoSystem(object):
     Class for control of Teo Systems Memory Tester
     TEO: Name=TS_MemoryTester SN=201428 Rev=2.4
 
-    TODO: put specs and basic explanation of what the system does here
-    fixed 500 MSamples/s sample rate for internal AWG and ADCs
-    +-10V output voltage amplitude, with 14 bit resolution (~5 mV), 50 ohm output
-    rise/fall time < 1 ns
-    ADC has 12 bit resolution
-    ADC memory is 256 MSamples per channel
-    ADC bandwidth is about 200 MHz
+    The main idea of this system is to combine a variety of measurement devices used for
+    modern memory or sensor routine test processes, yet leave the ability to connect
+    external equipment if needed.
+
+    There are two "modes", HF and LF
+
+    HF mode:
+        There is one DAC channel (AWG), whose output appears on the HFV port
+        256 MSamples can be stored in AWG memory
+        ±10V output voltage amplitude, with 14 bit resolution (~5 mV), 50 ohm output
+        rise/fall time < 1 ns
+        software can recall uploaded waveforms by name, to save data transfer time
+        There are two ADCs, one monitors the AWG voltage, and the other measures the voltage on the HFI port
+        ADCs have 12 bit resolution
+        ADC memory is 256 MSamples per channel
+        Fixed 500 MSamples/s sample rate for internal AWG and ADCs
+        There are several amplifiers on board.
+        HFV monitor has jumper selectable gain, normally set to read full scale ±10V.
+        HFI has two amplifiers whose gain can be contrelled by software
+        200 MHz bandwidth, signal is digitized by ADC channel and available on HF LIMITED BW port
+        lower gain, higher bandwidth amplifier (4 GHz) which goes to the HF FULL BW port.
+
+    LF mode:
+        TODO: elaborate this section
+        There is one DAC channel, one ADC channel
 
     designed for minimum DUT resistance 1kΩ, but shouldn't break easily if there is a short circuit
 
-    COM objects directly exposed:
+    We communicate with Teo software using the windows COM interface.
+    The provided COM classes are named like this:
     DriverID
     DeviceID
     DeviceControl
@@ -34,8 +53,10 @@ class TeoSystem(object):
     HF_Gain
     AWG_WaveformManager
 
-    Brings some of the most used methods that are inconveniently deep
-    in the object tree to the top level and gives them shorter names
+    In this class, we define some higher level functions that and appeal to our "pythonic" sensibilities.
+
+    Bring some of the most used methods that are inconveniently deep
+    in the object hierarchy to the top level and gives them shorter names
 
     Most of the functions I add start with a lowercase letter.
     All of TEOs functions are CapitalCamelCased, sometimes with underscores.
@@ -43,33 +64,18 @@ class TeoSystem(object):
     Adds content-addressability of waveforms for effortless replay
 
     Seems to handle re-initialization just fine.
-    You can make two instances and they will both work
-
+    You can make multiple instances and they will all work
 
     # TODO: does the class need to store any internal state?  should we make it BORG?
 
-    # TODO: write high level methods (e.g. pulse_and_capture..)
-
-    # TODO: only voltage amplitude is used for the hash, maybe we should hash the triggers as well
-
-    # TODO: how can we be aware of the TEO memory state?  do we care?
-
-    # TODO: TEO remembers waveforms that you upload by name
-            this is to minimize unecessary data transfer, which takes time
-            we should also have this class remember the waveforms in a similar way (done)
-            would be useful then to also have a method that synchronizes the memories
+    # TODO: write more high level methods (e.g. pulse_and_capture..), but nothing application specific
 
     # TODO: do all the commands work regardless of which mode we are in? e.g. waveform upload, gain setting
             how do we avoid issuing commands and expecting it to do something but we are in the wrong mode?
 
     # TODO: what happens when we send commands while the board is busy?
 
-    # TODO: my understanding is that there is an idle voltage level set by
-            LF_Measurement.LF_Voltage.SetValue(DClevel)
-            is this always applied when a waveform is not playing? (yes)
-            does that mean the instrument switches into LF mode when a waveform is not playing? (no)
-
-    # TODO: could wrap some of the functions only for the purpose of giving them signatures, docstrings,
+    # TODO: could wrap all of the functions only for the purpose of giving them signatures, docstrings,
             default arguments.
 
     # TODO: should we hide the entire COM interface in a container?  like
@@ -78,7 +84,9 @@ class TeoSystem(object):
             so on, but we can still access the com interface if needed.
             downside is that when we ask for support our code will be unrecognizable to Teo.
             therefore:
+
     # TODO: should we have some kind of a debug mode that prints out all the COM calls?
+            then we can send those to Teo for help
 
     # TODO: since there seem to be a several of situations that cause the output to go to the negative rail
             and blow up your device, make sure to document them here
@@ -86,9 +94,8 @@ class TeoSystem(object):
             1. on first initialization (Dispatch('TSX_HMan'))
             2. if you disconnect USB and plug it back in
 
-    # TODO: should we store a calibration to remove the offsets? monitor has a big offset and for
-            the other channel it depends a bit on the gain setting.
-
+    # TODO: store a calibration to remove the offsets and scale voltages.
+            monitor has a big offset and for the other channel it depends a bit on the gain setting.
     '''
 
     def __init__(self):
@@ -551,20 +558,15 @@ class TeoSystem(object):
         Get the data for both ADC channels for the last capture
         returns a dict of information
 
-        if wfm_data is True, return the programmed waveform and trigger data
         we return the programmed waveform data as well,
         which is useful because the monitor signal can have a lot of noise
 
         if raw is True, then keys 'HFV' and 'HFI' will be in the returned dict
         which are the ADC values before calibration/conversion/trimming
 
-        TODO: we are going to have a problem with this function if the number of shots was > 1!
-              should we np.repeat the programmed waveform and return that??
-              if memory is no issue, then yes
-
         TODO: Calibrate voltages and currents
               Store the calibrations here in the class definition
-        TODO: Align data with trigger?  we want to return arrays of the same length, even if the
+        TODO: How should we align data with trigger?  we want to return arrays of the same length, even if the
               triggers are not always on. We have two options:
               1. delete programmed voltage waveform and time waveform where trig1 is False
               2. pad measured waveform with np.nan where trig1 is False
@@ -696,6 +698,8 @@ class TeoSystem(object):
     @staticmethod
     def _compress_trigger(trigarray):
         '''
+        Not used at the moment
+
         The trigger arrays are equal in length to the main waveform
         but probably have far fewer transitions (maybe just one or two!)
         convert the data into the transitions?
@@ -773,9 +777,9 @@ class TeoSystem(object):
         '''
         Gets or sets the LF source voltage value
 
-        Voltage appears on the HFI port, not HFV, which is the reverse of HF mode.
+        In LF mode, the voltage appears on the HFI port, not HFV, which is the reverse of HF mode.
 
-        Also sets the idle level for HF mode..
+        This also sets the idle level for HF mode..  but doesn't switch to LF_mode or anything
 
         TODO: check that we are in the possible output voltage range
         TODO: rename?  LF_Voltage is the name of a class within LF_Measurement
@@ -828,7 +832,7 @@ class TeoSystem(object):
         '''
         Use internal LF mode to make a low-current measurement
         not tested
-        TODO: test
+        TODO: test this!
         '''
         self.LF_mode()
 
