895011a4e36267da2eb6fc92d2a3928550a100aa
Munoz <a.munoz@iwe.rwth-aachen.de>
2020-10-22 11:03:07 +0200

    fix digipot_test

--- ivtools/measure.py
+++ ivtools/measure.py
@@ -900,8 +900,8 @@ def digipot_test(plot=True):
 
     dp.set_bypass(0)
 
-    for w,Rnom in dp.Rmap.items():
-        dp.set_wiper(w) # Should have the necessary delay built in
+    for w,Rnom in enumerate(dp.R2list):
+        dp.set_state(wiper2=w) # Should have the necessary delay built in
         # Put 10 milliwatt through each resistor
         #A = np.sqrt(10e-3 * Rnom)
         #A = min(A, 5)
