0b4a4e50d7fd0a932770273bedc71b3274e4dea8
Erik <>
2020-11-06 18:34:17 +0100

    AUTOCOMMIT

--- firmware/EugenTempStage/PT1000_TReader/PT1000_TReader.ino
+++ firmware/EugenTempStage/PT1000_TReader/PT1000_TReader.ino
@@ -1,18 +1,16 @@
 //PT1000-Temperature READER with ARDUINO Micro
-
-
 #include <Wire.h>
-#include <Adafruit_GFX.h>
+//#include <Adafruit_GFX.h>
 #include <math.h>
 #include <CmdMessenger.h>
 #include <Adafruit_MCP4725.h> // Digital-Analog-Converter
-#include <LiquidCrystal.h> // LCD-Library
+//#include <LiquidCrystal.h> // LCD-Library
 //#include "Adafruit_LEDBackpack.h" // LED-Display
 #include <Filter.h> // Exponential-Filter
 
 //LCD-Konfiguration
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 10, d7 = 8;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const byte LCDa = 0x28;         //LCD address on I2C bus
+const char unit[] = {223,67,' ',' ',' ',' ' };  //degree Celsius + some spaces to fill display buffer
 
 // Attach a new CmdMessenger object to the default Serial port
 CmdMessenger cmdMessenger = CmdMessenger(Serial);
@@ -72,6 +70,8 @@ void AnalogOutput()
   
  // cmdMessenger.sendCmdArg(analogWriteValue);
     dac.setVoltage(analogWriteValue, false); //call dac-function "setVoltage"
+    LCDprintSP(5*analogWriteValue/4096);  //print setpoint on lcd and convert 12-bit value to human-readable
+    
  // cmdMessenger.sendCmdEnd();
 }
 
@@ -103,16 +103,51 @@ float pt_resistor(float volt_now, float volt_bridge)
   return pt_res;
   
 }
+// LCD functions
+void LCDclear() {
+  Wire.beginTransmission(LCDa);
+  Wire.write (0xFE);
+  Wire.write(0x51);
+  Wire.endTransmission();
+  delay(2);
+}
+
+// Set Cursor to specified position line 0 or 1, pos 0 ... 15;
+void LCDprint(char msg[], int line, byte pos) {
+  Wire.beginTransmission(LCDa);
+  Wire.write (0xFE);
+  Wire.write(0x45);
+  pos = pos &0xF; //limit pos to 15 cut upper 4 bits
+  line == 0 ? Wire.write(0x00+pos) : Wire.write(0x40+pos);   
+  Wire.write(msg);
+  Wire.endTransmission();
+  delay(2);
+}
+
+void LCDprintTemp(float temp) {
+  char buff[8];
+  dtostrf(temp,4,2,buff);    //convert float to ascii char array
+  LCDprint(strcat(buff,unit),0,6);
+}
+
+void LCDprintSP(float temp) {
+  char buff[8];
+  dtostrf(temp,4,2,buff);    //convert float to ascii char array
+  LCDprint(strcat(buff,unit),1,6);
+}
 
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  //matrix.begin(0x70);
   // initialize DAC
   dac.begin(0x62);
   // initialize the LCD's number of columns and rows:
-  lcd.begin(16,2); 
+  TWBR = 100000; //sets I2C speed to 100kHz very important set this after dac.begin
+  LCDclear();
+  delay(500);     // to show somesthing happend 
+  LCDprint("Temp:",0,0);
+  LCDprint("Set:",1,0);
   
   // Adds newline to every command
   cmdMessenger.printLfCr();
@@ -122,29 +157,11 @@ void setup() {
   
   //Start-Temperature (19*Celsius) / Voltage (0.8V)
   dac.setVoltage(655, false); 
-
-
-  // Frequency Tuning if necessary
-  //TCCR0B = TCCR0B & 0b11111000 |  0x01;  //B00000001; // for PWM frequency  TIMER 1
-  //TCCR1B = TCCR1B & 0b11111000 |  0x01;  //B00000001; // for PWM frequency about 32kHz  TIMER 2
-  //pinMode(9, OUTPUT);
-
-
 }
 
 void loop() {
   float sensorValue1 = analogRead(A1); //Analog-Pin 1, it reads the Output-Voltage from Bridge
   float sensorValue2 = analogRead(A2); //Checking for PowerSupply-Conection
-  
-  //Average-Filter
-  //float AverageTemp = 0;
-  //float MeasurementsAverage = 10;
-  //for (int i=0; i < MeasurementsAverage; ++i)
-  //{ 
-  //  AverageTemp += sensorValue1;    
-  //  delay(1);
-  //}
-  //AverageTemp /= MeasurementsAverage;
 
   // Convert the analog reading (which goes from 0 - 1023) to a voltage of (0 - 5V):
   float gain = 12.55; //Gain for Bridge-OPAmp
@@ -155,45 +172,24 @@ void loop() {
   //Power-Supply Voltage, should be above 5V
   float volt_powerSupply = (sensorValue2 * 5) / 1023;
 
-
   // Temperature Equation
   float temperature = (log(pt_resistor(volt_now, voltbridge) / 1000) / log(1.00385));
   float temp_log = temperature; 
-
   
   // Average Temperature Values
   FilteredTemperature.Filter(temp_log);
   float SmoothTemperature = FilteredTemperature.Current();
-
-  // LED-Matrix print
-  //matrix.println(temp_log);
-  //matrix.writeDisplay();
-  
-  // Turn on the LC-display:
-  lcd.display();
-
-  // Cleaning Display
-  for (int i = 0; i<=1; ++i){
-    for (int y = 0; y<=15; ++y){
-      lcd.setCursor(y,i);
-      lcd.write("");
-    }
-  }
   
   //Checking if Circuit has enough power
-  if (volt_powerSupply == NULL){
-    lcd.setCursor(2,1);
-    lcd.print("POWER OFF?");
+  if (volt_powerSupply < 4){
+    
+    LCDprint("check",0,6);
+    LCDprint("Power!",1,6);
     delay(40);
   } 
   else { 
-    lcd.setCursor(3,0);
-    lcd.print("   ");  // This covers up the previous value
-    lcd.setCursor(3,0);
-    lcd.print(SmoothTemperature);
-    lcd.setCursor(9,0);
-    lcd.print((char)223);
-    lcd.print("C");
+
+    LCDprintTemp(SmoothTemperature);
     delay(40);
   }  
 
