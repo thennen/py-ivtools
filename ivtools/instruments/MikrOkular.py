77ce685b7f7c2bf8b0d22cb26c98a6f08f7bd27d
mohr <mohr@iwe.rwth-aachen.de>
2022-04-14 20:15:43 +0200

    Added some documentation to the camera code.

--- ivtools/instruments/MikrOkular.py
+++ ivtools/instruments/MikrOkular.py
@@ -1,6 +1,10 @@
 import cv2 as cv
 
 class MikrOkular:
+    """
+    Handles the connection to the Bresser MikrOkular camera.
+    """
+    
     # Camera has three possible resolution settings, can give other values,
     # but these are mapped to the closest allowed setting
     # (width, height)
@@ -39,6 +43,31 @@ class MikrOkular:
     
     ###
     def __init__(self, camId = 0, settings = None, res = "high"):
+        """
+        Connects to the Bresser MikrOkular camera.
+
+        Parameters
+        ----------
+        camId : int, optional
+            If multiple cameras are connected to the PC, one can be selected
+            by a number. The default is 0. Multiple cameras are untested.
+        settings : dict, optional
+            Initial configuration of the camera setting (e.g. brightness).
+            If None, they remain whatever the camera is set to. The default is None.
+        res : str, optional
+            Initial resolution of the camera image.
+            Can be
+            "high" - (1920, 1080) px,
+            "mid" - (1280, 1024) px or
+            "low: - (1280, 720) px
+            The default is "high".
+
+        Returns
+        -------
+        None.
+
+        """
+        
         self.camId = camId
         
         # Check if a connection to this camera is already open, this happens
@@ -47,7 +76,9 @@ class MikrOkular:
         if camId in self.openedCams:
             self.camera =  MikrOkular.openedCams[camId]
         else:
-            self.camera = cv.VideoCapture(camId, cv.CAP_DSHOW) # TODO: DSHOW
+            self.camera = cv.VideoCapture(camId, cv.CAP_DSHOW)
+            # There seem to be different backends/drivers available
+            # for connecting to the camera, the default one does weird stuff.
             
         if not self.camera.isOpened():
             # This fails if the CamLabLite software is open
@@ -58,7 +89,8 @@ class MikrOkular:
         
         if not settings == None:
             self.setAllProperties(settings)
-            
+        
+        # TODO: For consistency, this should have a default like settings
         self.setResolution(res)
     
     # This doesn't do what one thinks a destructor does, which is also
@@ -67,12 +99,42 @@ class MikrOkular:
     #    self.camera.release()
     
     def close(self):
+        """
+        Closes the connection to the camera.
+
+        Returns
+        -------
+        None.
+
+        """      
         self.camera.release()
         del MikrOkular.openedCams[self.camId]
     
+    ##########################################################
     ### Functionality for getting images out of the camera ###
+    ##########################################################
    
     def showImg(self, scale = 0.3, gray = False):
+        """
+        Gets the current image from the camera and shows it in a window.
+        
+        To close the window, hit any key.
+        The image is by default scaled down, so it doesn't take up the whole
+        screen.
+
+        Parameters
+        ----------
+        scale : float, optional
+            Image is scaled down by this factor. This should be between
+            0 and 1. Larger values untested. The default is 0.3.
+        gray : bool, optional
+            Image is in grayscale if true. The default is False.
+
+        Returns
+        -------
+        None.
+
+        """
         frame = self.getImg(gray)
         
         # Scale down image for display purposes, it's basically screen
@@ -86,6 +148,28 @@ class MikrOkular:
         cv.destroyAllWindows()
         
     def liveView(self, scale = 0.3, gray = False):
+        """
+        Shows a live video from the camera.
+        
+        Close by hitting 'q'.
+        Video is scaled down by default, so it doesn't take up the whole
+        screen.
+        Settings can be changed, these are maintained after closing the
+        live view.
+
+        Parameters
+        ----------
+        scale : float, optional
+            Video is scaled down by this factor. This should be between
+            0 and 1. Larger values untested. The default is 0.3.
+        gray : bool, optional
+            Image is in grayscale if true. The default is False.
+
+        Returns
+        -------
+        None.
+
+        """
         # Somehow this is needed here, though it is also in the set/get
         # methods
         if not self.camera.isOpened():
@@ -142,6 +226,22 @@ class MikrOkular:
         cv.destroyAllWindows()
     
     def getImg(self, gray = False):
+        """
+        Returns the current camera image as a numpy array.
+
+        Parameters
+        ----------
+        gray : bool, optional
+            Image is in grayscale if true. The default is False.
+
+        Returns
+        -------
+        frame : ndarray
+            Matrix with the current pixel values of the camera. 3 dimensional
+            if color mode, otherwise 2 dimensional. Color channels are
+            (blue, green, red).
+
+        """
         if not self.camera.isOpened():
             raise Exception("Connection to camera was closed!")
         
@@ -161,16 +261,53 @@ class MikrOkular:
         return frame
 
     def saveImg(self, path, gray = False):
+        """
+        Writes the current camera image to disc.
+
+        Parameters
+        ----------
+        path : str
+            File to write image to. Ending determines file type.
+        gray : bool, optional
+            Image is in grayscale if true. The default is False.
+
+        Returns
+        -------
+        None.
+
+        """
         frame = self.getImg(gray)
         
         cv.imwrite(path, frame)
     
+    #################################
     ### Configure camera settings ###
+    #################################
     
+    # TODO: does having that default make sense?
     def setResolution(self, res = "high"):
+        """
+        Sets the camera resolution.
+
+        Parameters
+        ----------
+        res : str, optional
+            Can be
+            "high" - (1920, 1080) px,
+            "mid" - (1280, 1024) px or
+            "low: - (1280, 720) px
+            The default is "high".
+
+        Returns
+        -------
+        None.
+
+        """
         if not self.camera.isOpened():
             raise Exception("Connection to camera was closed!")
-        # check here
+        
+        if not res in self.resolutions.keys():
+            raise Exception("Resolution must be 'low', 'mid' or 'high'!")
         
         width = self.resolutions[res.casefold()][0]
         height = self.resolutions[res.casefold()][1]
@@ -182,6 +319,15 @@ class MikrOkular:
             raise Exception("Could not set resolution!")
         
     def getResolution(self):
+        """
+        Returns the current camera resolution in pixels.
+
+        Returns
+        -------
+        (width, height) : int
+            Current resolution in pixels.
+
+        """
         if not self.camera.isOpened():
             raise Exception("Connection to camera was closed!")
         width = self.camera.get(cv.CAP_PROP_FRAME_WIDTH)
@@ -190,6 +336,25 @@ class MikrOkular:
         return (int(width), int(height))
     
     def setPropertyNative(self, prop, value):
+        """
+        Sets one camera setting, using the cameras units.
+
+        Parameters
+        ----------
+        prop : str
+            Setting to change.
+            Possible values are: 
+              "brightness", "contrast", "hue", "saturation", "gamma",
+              "sharpness", "exposure"
+        value : int
+            The value to be set. Possible values are in the cameras own unit,
+            and somewhat strange.
+
+        Returns
+        -------
+        None.
+
+        """
         # Check if property exists
         prop = prop.casefold()
         if not prop in self.codes.keys():
@@ -211,6 +376,23 @@ class MikrOkular:
             raise Exception("Could not set property!")
         
     def getPropertyNative(self, prop):
+        """
+        Returns one camera setting, in the cameras units.
+
+        Parameters
+        ----------
+        prop : str
+            Setting to return.
+            Possible values are: 
+              "brightness", "contrast", "hue", "saturation", "gamma",
+              "sharpness", "exposure"
+
+        Returns
+        -------
+        int
+            The current setting.
+
+        """
         # Check if property exists
         prop = prop.casefold()
         if not prop in self.codes.keys():
@@ -224,6 +406,24 @@ class MikrOkular:
         return int(value)
          
     def setProperty(self, prop, value):
+        """
+        Sets one camera setting, as percent of the possible range.
+
+        Parameters
+        ----------
+        prop : str
+            Setting to change.
+            Possible values are: 
+              "brightness", "contrast", "hue", "saturation", "gamma",
+              "sharpness", "exposure"
+        value : float
+            Should be in the range 0 to 1.
+
+        Returns
+        -------
+        None.
+
+        """
         # TODO: Check value here to give clearer error?
         prop = prop.casefold()
         value = self.ranges[prop][0] + (self.ranges[prop][1]-self.ranges[prop][0]) * value
@@ -231,6 +431,23 @@ class MikrOkular:
         self.setPropertyNative(prop, value)
     
     def getProperty(self, prop):
+        """
+        Returns one camera setting, as percent of the possible range.
+
+        Parameters
+        ----------
+        prop : str
+            Setting to return.
+            Possible values are: 
+              "brightness", "contrast", "hue", "saturation", "gamma",
+              "sharpness", "exposure"
+
+        Returns
+        -------
+        value : float
+            Current value in the range 0 to 1.
+
+        """
         prop = prop.casefold()
         value = self.getPropertyNative(prop)
         value = (value - self.ranges[prop][0])/(self.ranges[prop][1]-self.ranges[prop][0])
@@ -238,6 +455,15 @@ class MikrOkular:
         return value
     
     def getAllProperties(self):
+        """
+        Returns a dictionary with the values of all settings.
+
+        Returns
+        -------
+        props : dict
+            Current settings.
+
+        """
         props = {}
         for p in self.codes.keys():
             props.update({p: self.getProperty(p)})
@@ -245,5 +471,19 @@ class MikrOkular:
         return props
     
     def setAllProperties(self, props):
+        """
+        Sets all settings from a dictionary.
+
+        Parameters
+        ----------
+        props : dict
+            Should contain 'Setting':'Value' pairs. Value is between 0 and 1.
+            Only tested when all settings are present.
+
+        Returns
+        -------
+        None.
+
+        """
         for p, v in props.items():
             self.setProperty(p, v)
